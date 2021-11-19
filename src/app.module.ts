efc04d22e4a63c2e052815b53068d29bbaf82a53
Luiz Rogerio Bengozi <lrbengozi@gmail.com>
2021-11-19 14:04:55 -0300

    Change mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://lrmongo:lpassrword@cluster0.hl84w.mongodb.net/test',
+      'mongodb+srv://lrmongo:lpassrword@messenger-lr.hl84w.mongodb.net/test',
     ),
     UsersModule,
   ],
